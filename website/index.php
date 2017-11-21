cbafe42b27ec779a98e459eab5cc039e60b95a39
vcburnett <victorburnett@Victors-iMac-2.local>
2017-11-21 01:43:13 -0500

    Home page 1

---
+++ website/index.php
@@ -0,0 +1,188 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta content="width=device-width, initial-scale=1.0, maximum-scale=1" name="viewport" />
+<title>Maïté Indacochéa</title>
+<link rel="shortcut icon" href="assets/images/favicon.png"/>
+
+<link href="https://fonts.googleapis.com/css?family=Reenie+Beanie|Titillium+Web:400,600,700" rel="stylesheet">
+
+<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+	<header>
+		<div id="header-logo">
+			<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 491.1 304.6" style="enable-background:new 0 0 491.1 304.6;" xml:space="preserve">
+				<g>
+					<path class="maite-logo" d="M91.2,298.9v5.7c-18.2-2.5-33-9.9-44.4-22.1c-11.4-12.3-17.1-25.8-17.1-40.8c0-7.8,1.3-17,3.8-27.4c2.5-10.4,3.8-18.5,3.8-24.3c0-7.8-3.3-15.2-9.9-22c-6.6-6.8-15.7-11-27.4-12.5v-6.7c11.7-1.5,20.8-5.7,27.4-12.4c6.6-6.8,9.9-14.1,9.9-22.1c0-5.8-1.3-13.8-3.8-24.2c-2.5-10.3-3.8-19.5-3.8-27.5c0-14.8,5.7-28.4,17.1-40.7C58.2,9.8,73,2.4,91.2,0v5.7c-12.7,3.1-22.1,8-28,15c-5.9,6.9-8.9,14.5-8.9,22.8c0,6.4,1.2,14.9,3.7,25.4c2.5,10.5,3.7,19.5,3.7,26.9c0,11-4,21.7-12.1,32c-8.1,10.4-20.1,18.4-36.1,24c15.7,5.8,27.6,13.9,35.9,24.5c8.2,10.6,12.3,21.4,12.3,32.4c0,7.4-1.2,16.4-3.7,26.9s-3.7,19-3.7,25.4c0,8.3,3,15.9,8.9,22.8C69.1,290.7,78.4,295.7,91.2,298.9z"/>
+					<path class="maite-logo" d="M244,181l48.9-118.7h73.3v4.9h-5.8c-5.3,0-9.6,0.9-12.7,2.8c-2.2,1.2-4,3.3-5.3,6.2c-1,2.1-1.5,7.7-1.5,16.6v118.7c0,9.2,0.5,15,1.5,17.3s3,4.3,6,6c3,1.7,7,2.6,12.1,2.6h5.8v4.9h-94v-4.9h5.8c5.3,0,9.6-0.9,12.7-2.8c2.2-1.2,4-3.4,5.3-6.4c1-2.1,1.5-7.7,1.5-16.7V78.1l-69,164.2h-3.2l-70.1-163v126.8c0,8.8,0.2,14.3,0.7,16.3c1.1,4.4,3.7,8,7.5,10.8c3.9,2.8,9.9,4.2,18.1,4.2v4.9h-61.6v-4.9h1.9c4,0.1,7.7-0.6,11.2-1.9c3.5-1.4,6.1-3.2,7.8-5.5s3.1-5.5,4.1-9.7c0.2-1,0.3-5.5,0.3-13.5V92.8c0-9.1-0.5-14.8-1.5-17.1c-1-2.3-3-4.2-6-6s-7-2.6-12.1-2.6h-5.7v-4.9h73.6L244,181z"/>
+					<path class="maite-logo" d="M400,5.7V0c18.2,2.4,33,9.7,44.4,22c11.4,12.3,17.1,25.9,17.1,40.9c0,7.8-1.3,16.9-3.8,27.3c-2.5,10.4-3.8,18.4-3.8,24.2c0,7.8,3.3,15.2,9.9,22c6.6,6.8,15.7,11,27.4,12.5v6.7c-11.7,1.5-20.8,5.7-27.4,12.5c-6.6,6.8-9.9,14.1-9.9,22c0,5.8,1.3,13.9,3.8,24.3c2.5,10.4,3.8,19.5,3.8,27.4c0,14.8-5.7,28.4-17.1,40.7c-11.4,12.3-26.2,19.7-44.4,22.2v-5.7c12.7-3.2,22.1-8.2,28-15c5.9-6.9,8.9-14.5,8.9-22.9c0-6.4-1.3-14.9-3.8-25.4c-2.5-10.5-3.8-19.4-3.8-26.7c0-11.1,4.1-21.8,12.2-32.2c8.1-10.3,20.2-18.4,36.2-24c-15.7-5.8-27.7-13.9-35.9-24.5c-8.3-10.6-12.4-21.4-12.4-32.4c0-7.4,1.3-16.4,3.8-26.9c2.5-10.5,3.8-19,3.8-25.4c0-8.3-3-15.9-8.9-22.8C422,13.8,412.7,8.8,400,5.7z"/>
+				</g>
+			</svg>
+		</div>
+		<nav>
+			<ul>
+				<li>accueil</li>
+				<li>sur moi</li>
+				<li>mon cv</li>
+				<li>contact</li>
+			</ul>
+		</nav>
+	</header>
+
+	<section id="hero">
+		<div id="header-content">
+			<h1>Maïté Indacochéa</h1>
+			<div id="header-links">
+				<a href="javascript:void(0);">Rédactrice</a>
+				<a href="javascript:void(0);">Traductrice</a>
+			</div>
+			<p>Langagière polyvalente qui permet aux individus et aux entreprises de produire des communications riches, pertinentes et linguistiquement irréprochables, adaptées au Web.</p>
+		</div>
+	</section>
+
+	<div id="text">
+		<div id="anti-skew">
+			<div class="internal-wrap">
+				<div id="txt-left">
+					<p>Une plume plurielle et des textes d’exception, au service des entrepreneurs d’ici.</p>
+				</div>
+				<div id="txt-right">
+					<p>Je suis une langagière polyvalente qui permet aux individus et aux entreprises de produire des communications riches, pertinentes et linguistiquement irréprochables, adaptées au Web.</p>
+					<p>Si mon parcours scolaire et la pratique de la traduction à titre de traductrice agréée (OTTIAQ) autonome m’ont permis d’acquérir la rigueur linguistique et la souplesse stylistique de cette discipline, c’est une heureuse bifurcation professionnelle vers le marketing digital qui confère à mon écriture sa richesse sémantique et l’approche méthodique du SEO, ainsi que les formulations efficaces et accrocheuses du marketing.</p>
+					<p>Passionnée de tout, je m’inspire de mes expériences diversifiées pour assurer la rédaction et la traduction de contenu unique, qualifié et performant.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="quote">
+		<div id="quote-skew">
+			<div id="quote-wrap">
+				<h2>« Nous savons manier les lettres, apprenons à manier les chiffres. Business is business. »</h2>
+				<p id="author">– Betty Cohen, trad. a. (Circuit #118, hiver 2013)</p>
+			</div>
+		</div>
+	</div>
+
+	<div id="cv">
+		<div class="internal-wrap">
+			<h4>Expériences<br>Professionelles</h4>
+			<div id="main-cv">
+				<div class="cv-logo">
+
+				</div>
+				<div class="cv-content">
+					<h2>Rédactrice Web / Junior SEO</h2>
+					<div class="div-line"></div>
+					<h3 class="cv-company">Konversion <span class="date">Novembre 2016 à aujourd'hui</span></h3>
+					<div class="div-line"></div>
+					<ul>
+						<li>Rédaction de contenu (EN/FR) dans le cadre de campagnes SEM</li>
+						<li>Élaboration et mise en œuvre de stratégies de contenu</li>
+						<li>Création de contenu SEO : guest blogging, balises et contenu on-site</li>
+						<li>Mise en place de stratégies de linkbuilding</li>
+						<li>Mise en place de stratégies marketing multimédia créatives</li>
+						<li>Révision et correction de contenus en ligne</li>
+						<li>Traduction de l’anglais au français</li>
+					</ul>
+					<p class="cv-section-title">Formation continue</p>
+					<ul>
+						<li>Cours de maîtrise en Marketing électronique au HEC Montréal</li>
+						<li>Certification Google Analytics</li>
+						<li>Formations à l’interne : SEM (AdWords), SEO, etc.</li>
+						<li>Participation au SEO camps (Association DoYouSEO)</li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="secondary-cv">
+				<div class="cv-logo">
+
+				</div>
+				<div class="cv-content">
+					<h2>Rédactrice Web</h2>
+					<div class="div-line"></div>
+					<h3 class="cv-company">BJMédia <span class="date">Janvier à novembre 2016</span></h3>
+					<div class="div-line"></div>
+					<ul>
+						<li>Rédaction de contenu on-site (EN/FR)</li>
+						<li>Gestion de projets (sites web simples – plateforme WordPress)</li>
+						<li>Élaboration de stratégies de marketing de contenu inbound</li>
+						<li>Création de contenu SEO : guest blogging, balises et contenu on-site</li>
+						<li>Mise en place de stratégies de linkbuilding</li>
+						<li>Révision et correction de contenus en ligne</li>
+						<li>Traduction de l’anglais au français</li>
+					</ul>
+					<p class="cv-section-title">Formation continue</p>
+					<ul>
+						<li>Formation de communication non-violente</li>
+						<li>Formations à l’interne : SEO, gestion de projet</li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="secondary-cv">
+				<div class="cv-logo">
+
+				</div>
+				<div class="cv-content">
+					<h2>Rédactrice Web</h2>
+					<div class="div-line"></div>
+					<h3 class="cv-company">OTTIAQ <span class="date">Mars à novembre 2017</span></h3>
+					<div class="div-line"></div>
+					<ul>
+						<li>Conférence à l’occasion du 25e congrès de l’Ordre : « S’épanouir sur le marché linguistique, par où commencer? » avec Mathieu Gautier, trad. a. et Président d’Élite Communication</li>
+						<li>Participer à l’organisation et veiller au bon déroulement de cet événement international</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+	<div id="contact">
+		<div id="contact-wrap">
+			<p>Pour toutes questions ou demande d'information</p>
+			<p class="txt-ecrivez">écrivez-moi!</p>
+			<form>
+
+			</form>
+			<div id="contact-txt">
+				<a href="javascript:void(0);" class="btn-linkedin">
+					<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 148.3 36" style="enable-background:new 0 0 148.3 36;" xml:space="preserve">
+						<path class="st0" d="M111.4,13.5h5.3v17.2h-5.3V13.5zM114.1,5c-1.7,0-3.1,1.4-3.1,3.1c0,1.7,1.4,3.1,3.1,3.1c1.7,0,3.1-1.4,3.1-3.1C117.2,6.3,115.8,5,114.1,5z M130.3,13.1c-2.6,0-4.3,1.4-5.1,2.8h-0.1v-2.3h-5.1v17.2h5.3v-8.5c0-2.2,0.4-4.4,3.2-4.4c2.7,0,2.8,2.6,2.8,4.6v8.4h5.3v-9.4C136.7,16.6,135.7,13.1,130.3,13.1z"/>
+					</svg>
+				</a>
+				<p><span class="contact-label">t</span> (514) 924-9639</p>
+				<p><span class="contact-label">e</Span> maite@maitxt.ca</p>
+				<p>Longueuil, QC</p>
+			</div>
+		</div>
+	</div>
+
+	<footer>
+		<div id="ft-logo-wrap">
+			<div id="ft-logo">
+				<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 491.1 304.6" style="enable-background:new 0 0 491.1 304.6;" xml:space="preserve">
+					<g>
+						<path class="maite-logo" d="M91.2,298.9v5.7c-18.2-2.5-33-9.9-44.4-22.1c-11.4-12.3-17.1-25.8-17.1-40.8c0-7.8,1.3-17,3.8-27.4c2.5-10.4,3.8-18.5,3.8-24.3c0-7.8-3.3-15.2-9.9-22c-6.6-6.8-15.7-11-27.4-12.5v-6.7c11.7-1.5,20.8-5.7,27.4-12.4c6.6-6.8,9.9-14.1,9.9-22.1c0-5.8-1.3-13.8-3.8-24.2c-2.5-10.3-3.8-19.5-3.8-27.5c0-14.8,5.7-28.4,17.1-40.7C58.2,9.8,73,2.4,91.2,0v5.7c-12.7,3.1-22.1,8-28,15c-5.9,6.9-8.9,14.5-8.9,22.8c0,6.4,1.2,14.9,3.7,25.4c2.5,10.5,3.7,19.5,3.7,26.9c0,11-4,21.7-12.1,32c-8.1,10.4-20.1,18.4-36.1,24c15.7,5.8,27.6,13.9,35.9,24.5c8.2,10.6,12.3,21.4,12.3,32.4c0,7.4-1.2,16.4-3.7,26.9s-3.7,19-3.7,25.4c0,8.3,3,15.9,8.9,22.8C69.1,290.7,78.4,295.7,91.2,298.9z"/>
+						<path class="maite-logo" d="M244,181l48.9-118.7h73.3v4.9h-5.8c-5.3,0-9.6,0.9-12.7,2.8c-2.2,1.2-4,3.3-5.3,6.2c-1,2.1-1.5,7.7-1.5,16.6v118.7c0,9.2,0.5,15,1.5,17.3s3,4.3,6,6c3,1.7,7,2.6,12.1,2.6h5.8v4.9h-94v-4.9h5.8c5.3,0,9.6-0.9,12.7-2.8c2.2-1.2,4-3.4,5.3-6.4c1-2.1,1.5-7.7,1.5-16.7V78.1l-69,164.2h-3.2l-70.1-163v126.8c0,8.8,0.2,14.3,0.7,16.3c1.1,4.4,3.7,8,7.5,10.8c3.9,2.8,9.9,4.2,18.1,4.2v4.9h-61.6v-4.9h1.9c4,0.1,7.7-0.6,11.2-1.9c3.5-1.4,6.1-3.2,7.8-5.5s3.1-5.5,4.1-9.7c0.2-1,0.3-5.5,0.3-13.5V92.8c0-9.1-0.5-14.8-1.5-17.1c-1-2.3-3-4.2-6-6s-7-2.6-12.1-2.6h-5.7v-4.9h73.6L244,181z"/>
+						<path class="maite-logo" d="M400,5.7V0c18.2,2.4,33,9.7,44.4,22c11.4,12.3,17.1,25.9,17.1,40.9c0,7.8-1.3,16.9-3.8,27.3c-2.5,10.4-3.8,18.4-3.8,24.2c0,7.8,3.3,15.2,9.9,22c6.6,6.8,15.7,11,27.4,12.5v6.7c-11.7,1.5-20.8,5.7-27.4,12.5c-6.6,6.8-9.9,14.1-9.9,22c0,5.8,1.3,13.9,3.8,24.3c2.5,10.4,3.8,19.5,3.8,27.4c0,14.8-5.7,28.4-17.1,40.7c-11.4,12.3-26.2,19.7-44.4,22.2v-5.7c12.7-3.2,22.1-8.2,28-15c5.9-6.9,8.9-14.5,8.9-22.9c0-6.4-1.3-14.9-3.8-25.4c-2.5-10.5-3.8-19.4-3.8-26.7c0-11.1,4.1-21.8,12.2-32.2c8.1-10.3,20.2-18.4,36.2-24c-15.7-5.8-27.7-13.9-35.9-24.5c-8.3-10.6-12.4-21.4-12.4-32.4c0-7.4,1.3-16.4,3.8-26.9c2.5-10.5,3.8-19,3.8-25.4c0-8.3-3-15.9-8.9-22.8C422,13.8,412.7,8.8,400,5.7z"/>
+					</g>
+				</svg>
+			</div>
+			<div id="ft-txt">
+				<p>Maïté Indacochéa</p>
+			</div>
+		</div>
+		<div id="copyright">
+			<p>©2017 Copyright. Tous droits réservés.</p>
+		</div>
+	</footer>
+
+</body>
+</html>
